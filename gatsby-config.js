20118ebed1a700b2dee410ebfab669e466121a4e
B0hr <trenson.gilles@gmail.com>
2021-03-18 20:47:59 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  siteMetadata: {
+    title: "Gilles.is",
+  },
+  plugins: [
+    "gatsby-plugin-theme-ui",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
