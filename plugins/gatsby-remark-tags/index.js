330714b1414cb7d085923a97b56a15182a6886b1
B0hr <trenson.gilles@gmail.com>
2021-04-07 18:46:16 +0200

    updated gatsby-remark-tags

--- plugins/gatsby-remark-tags/index.js
+++ plugins/gatsby-remark-tags/index.js
@@ -1,14 +1,18 @@
+"use strict"
+
 const visit = require("unist-util-visit")
 const { paragraph, text, link } = require("mdast-builder")
 const toString = require("mdast-util-to-string")
 
-module.exports = ({ markdownAST }, pluginOptions) => {
+module.exports = ({
+    markdownAST 
+}, pluginOptions = {}) => {
     const regex = /#\w*/g
-    visit(markdownAST, "text", (node) => {
+    visit(markdownAST, "text", node => {
         
         if (!regex.test(node.value)) return
 
-        const html = node.value.replaceAll(/#\w+/g, (m) => 
+        const html = node.value.replace(/#\w+/g, (m) => 
             `<a target="${m} noreferer">${m}</a>`
         )
         
@@ -16,5 +20,4 @@ module.exports = ({ markdownAST }, pluginOptions) => {
         node.children = undefined
         node.value = html
     })
-    return markdownAST
 }
